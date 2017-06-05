b7a5b8d3fef1f5c93034bf59e3d666b8f8f378e5
Andrew Yurkevich <borgh1989@mail.ru>
2017-06-05 12:52:17 +0300

    second commit

--- src/main/java/com/borgh1989/servlet/MainServlet.java
+++ src/main/java/com/borgh1989/servlet/MainServlet.java
@@ -1,7 +1,30 @@
 package com.borgh1989.servlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
 /**
  * Created by Андрей on 05.06.2017.
  */
-public class MainServlet {
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("myPage.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+
+
 }
---
+++ src/main/webApp/myPage.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Андрей
+  Date: 05.06.2017
+  Time: 12:48
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Simple Servlet</title>
+</head>
+<body>
+    <h1>Hello Servlet</h1>
+</body>
+</html>
